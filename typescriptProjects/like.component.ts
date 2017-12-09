e6b184464c490c851470bf74cfa21c5254d23e57
Charles Burch <charlesburch@CBMacBook-Pro.local>
2017-12-09 15:52:59 -0600

    code refactored onclick method

---
+++ typescriptProjects/like.component.ts
@@ -0,0 +1,16 @@
+export class LikeComponent{
+     
+    constructor(public likesCount:number,public isSelected:boolean){
+    }
+
+    onClick(){
+        if(this.isSelected){
+            this.likesCount--;
+            this.isSelected = false;
+        }
+        else{
+            this.likesCount++;
+            this.isSelected = true;
+        }
+    }
+}
