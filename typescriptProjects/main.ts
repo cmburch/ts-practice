dfa118cdce626de0b70f4f7df029af7a2f87f79b
Charles Burch <charlesburch@CBMacBook-Pro.local>
2017-12-09 11:41:57 -0600

    type assertion added

--- typescriptProjects/main.ts
+++ typescriptProjects/main.ts
@@ -2,5 +2,16 @@ function log(message){
     console.log(message);
 }
 
-var message = 'Hello World';
+let message = 'Hello World';
 log(message);
+
+//testing vs code
+let count = 5;
+count = 'a';
+
+//type assertion has two ways to declare
+let message1;
+message1 = 'abc';
+
+let endswithC = (<string>message).endsWith('c');
+let alternativeWay = (message1 as string).endsWith('c');
